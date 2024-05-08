7c3521b458f795a1f9bf62a5d0f1147cf60a4beb
Ruslan Iskhakov <ruslan.iskhak94@gmail.com>
2024-05-08 19:57:15 +0300

    Added apps users, reviews and api. Reassigned User model, registered it in users.admin

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -21,6 +21,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'users',
+    'reviews',
+    'api'
 ]
 
 MIDDLEWARE = [
@@ -94,6 +97,8 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
+
+AUTH_USER_MODEL = 'users.MyUser'
 
 
 # Static files (CSS, JavaScript, Images)
