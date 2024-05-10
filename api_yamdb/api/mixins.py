37775b14b3e08023e08bde36562b85930274f703
tat-romanenko@mail.ru <tat-romanenko@mail.ru>
2024-05-10 17:29:51 +0300

    user_feature/category

--- api_yamdb/api/mixins.py
+++ api_yamdb/api/mixins.py
@@ -2,7 +2,6 @@ from rest_framework.filters import SearchFilter
 from rest_framework.mixins import (CreateModelMixin,
                                    DestroyModelMixin,
                                    ListModelMixin)
-from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from rest_framework.viewsets import GenericViewSet
 
 from api.permissions import IsAdminOrReadOnly
