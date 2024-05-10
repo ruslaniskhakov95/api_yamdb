516ddf4b81bc91a6a3c541eb28cac6500a456ee3
Ruslan Iskhakov <ruslan.iskhak94@gmail.com>
2024-05-10 22:00:22 +0300

    updated permissions

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -12,4 +12,5 @@ class IsAuthorOrReadOnly(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
         if request.method == 'GET':
             return True
-        return obj.author == request.user
+        return (obj.author == request.user
+                or request.user.is_admin or request.user.is_staff)
