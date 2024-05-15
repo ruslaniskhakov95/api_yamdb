06c6309dcfd69253ccaf2b947fb81203b3a3c8ac
Ruslan Iskhakov <ruslan.iskhak94@gmail.com>
2024-05-15 08:12:48 +0300

    api/v1/ pushed into root urls.py

--- api_yamdb/users/urls.py
+++ api_yamdb/users/urls.py
@@ -10,7 +10,7 @@ router_api_v1.register('users', CustomUserViewSet, basename='users')
 
 
 urlpatterns = [
-    path('api/v1/auth/token/', GetTokenView.as_view(), name='get_token'),
-    path('api/v1/auth/signup/', SignUpView.as_view(), name='signup'),
-    path('api/v1/', include(router_api_v1.urls)),
+    path('auth/token/', GetTokenView.as_view(), name='get_token'),
+    path('auth/signup/', SignUpView.as_view(), name='signup'),
+    path('', include(router_api_v1.urls)),
 ]
